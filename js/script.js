ef7c49da656fc70dabe3c7f0686f87a002e8f1b5
Marcos Felipe <marcosfe54321@gmail.com>
2026-07-09 15:22:56 -0300

    feat: adicionar telas em HTML/CSS do Empreenda+ Elas

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ====================================
+// EMPREENDA MAIS ELAS - SCRIPT
+// ====================================
+
+// Arquivo JavaScript para interações futuras
+// A ser desenvolvido nas próximas etapas
+
+console.log("Página carregada com sucesso!");
